d93a5c09f088cffa825ed90a149141e6b056f160
Paweł Kołodziejczak <modwebpro@gmail.com>
2024-12-05 12:38:43 +0100

    Update rateStar.js

--- rateStar.js
+++ rateStar.js
@@ -1,96 +1,118 @@
-export class rateStar {
-  constructor() {
-    if (!this._setVars()) return;
+class rateStar {
+  constructor(el) {
+    this.init(el)
+  }
+
+  init(el) {
+    if (!this._setVars(el)) return;
     this._setEvents();
   }
 
-  _setVars() {
-    let _this = this;
+  _setVars(el) {
+    this._el = el
+    if (!this._el) return false;
 
-    _this._el = document.getElementById('rate')
-    if (!this._el || !this._el.children) return false
-    
-    _this._docId = parseFloat(this._el.dataset.id) || false
-    if (!this._docId) return false
+    this._docId = parseFloat(this._el.getAttribute('data-id')) || false;
+    if (!this._docId) return false;
 
-    _this._rate = parseFloat(this._el.dataset.rate) || 0
+    this.btn = this._el.parentNode.querySelector('button[data-save]')
 
-    _this._starTotal = this._el.children.length - 2
+    this._rate = parseFloat(this._el.getAttribute('data-rate')) || parseFloat(localStorage.getItem(this._docId)) || 0;
+    this._key = 0
+    this._starTotal = this._el.children.length - 1;
 
     return true;
   }
 
   _setEvents() {
-    this._setStars(this._rate, this._starTotal);
-
-    if (this._verify()) {
-      this._setRate();
-      this._cleanStars();
-    }
-
+    this._setStars(this._rate, this._starTotal)
+    if (!this.btn) return
+    this._setRate()
+    this.storeData()
+    this._cleanStars()
   }
 
   _setStars(rate = 0, total = 0) {
     const starPercentage = (rate / total) * 100;
     const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
 
-    this._el.firstElementChild.style.width = starPercentageRounded;
-    this._el.firstElementChild.setAttribute('data-width', starPercentageRounded);
-    this._el.querySelector('span').innerHTML = `${rate}/${total}`;
+    this._el.children[0].style.width = starPercentageRounded;
+    this._el.children[0].setAttribute('data-width', starPercentageRounded);
+    let description = document.body.querySelector('data-rate_desc')
+    if (description) description.textContent = `${rate}/${total}`;
   }
 
   _setRate() {
-    each(this._el.children, (key, val) => {
+    _each(this._el.children, (key, val) => {
       if (key < 1 || key >= 6) return;
 
       val.addEventListener('mouseenter', () => {
-        if (!this._verify()) return;
-
         val.style.cursor = 'pointer';
-
         switch (key) {
           case 1:
-            this._el.firstElementChild.style.width = '20%';
+            this._el.children[0].style.width = '20%';
             break;
           case 2:
-            this._el.firstElementChild.style.width = '40%';
+            this._el.children[0].style.width = '40%';
             break;
           case 3:
-            this._el.firstElementChild.style.width = '60%';
+            this._el.children[0].style.width = '60%';
             break;
           case 4:
-            this._el.firstElementChild.style.width = '80%';
+            this._el.children[0].style.width = '80%';
             break;
           case 5:
-            this._el.firstElementChild.style.width = '100%';
+            this._el.children[0].style.width = '100%';
             break;
         }
-      });
+      })
 
       val.addEventListener('click', (e) => {
-        
-        // save to back-end
-        !this._verify() ? e.preventDefault() : sendPost({
-          url: 'RateController.php',
-          data: `rate=1&art=${this._docId}&val=${key}`,
-          cb: (xhr) => {
-            localStorage.setItem(this._docId, key);
-            this._setStars(parseFloat(xhr.response), this._starTotal);
-          }
-        })
+        this._rate = key
+        this._key = key
+        localStorage.setItem(this._docId, key)
+        this._setStars(key, this._starTotal)
+      })
+    })
+  }
 
-      });
-    });
+  _get(name) {
+    return this[name]
+  }
+
+  storeData() {
+    this.btn.addEventListener('click', (e) => {
+      e.preventDefault()
+      if (!this._get('_key')) return alert('Please select a rating from 1-5')
+      e.target.style.display = 'none'
+      _render(this._el.parentNode.parentNode, `<p class="rate__plugin--thx">Thank you for your rating.</p>`)
+      sendPost({
+        url: '/api/rate_add/',
+        data: JSON.stringify({art_id: this._docId, set_rate: this._key}),
+        cb: (xhr) => {
+          let response = JSON.parse(xhr.response)
+          localStorage.setItem(this._docId, this._key)
+          this._rate = parseFloat(response.rate)
+          this._setStars(this._rate, this._starTotal);
+        }
+      })
+    })
   }
 
   _verify() {
-    return localStorage.getItem(this._docId)) ? false : true;
+    try {
+      return localStorage.getItem(this._docId)
+    } catch (e) {
+      return true;
+    }
   }
 
   _cleanStars() {
     this._el.parentNode.parentNode.addEventListener('mouseleave', () => {
-      this._el.firstElementChild.style.width = this._el.firstElementChild.dataset.width;
-      !this._verify() && each(this._el.children, (key, val) => { val.style.cursor = '' });
+      this._el.children[0].style.width = this._el.children[0].getAttribute('data-width');
+      !this._verify() ? _each(this._el.children, (key, val) => val.style.cursor = '') : '';
     });
   }
 }
+
+export default rateStar
