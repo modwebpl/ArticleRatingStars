5d426c8cacdc9c2b8ba84f4f9e3ae3e79d0595d3
Paweł Kołodziejczak <modwebpro@gmail.com>
2024-12-05 12:43:21 +0100

    Create rate_get.php

---
+++ rate_get.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+require_once __DIR__ . '/../../wp-load.php';
+require_once __DIR__ . '/../lib/FormRequest.php';
+global $wpdb;
+
+$request = new FormRequest('json');
+if(!$request->get('art_id')) die(json_encode(['status' => false, 'msg' => 'invalid token']));
+$results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}art_rate WHERE art_id = ".intval($request->get('art_id')), OBJECT);
