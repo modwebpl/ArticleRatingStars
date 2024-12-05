b75071a33393a7df4485ea96652e0ddaabe2bfdd
Paweł Kołodziejczak <modwebpro@gmail.com>
2024-12-05 12:46:51 +0100

    Update rate_get.php

--- rate_get.php
+++ rate_get.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/../../wp-load.php';
-require_once __DIR__ . '/../lib/FormRequest.php';
+require_once __DIR__ . '/../../FormRequest.php';
 global $wpdb;
 
 $request = new FormRequest('json');
